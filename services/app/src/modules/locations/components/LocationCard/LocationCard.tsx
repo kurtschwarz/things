e971d32d50aff1db66d1f453f1e5a31474c8cc2e
Kurt Schwarz <kurt@unkn.co>
2023-04-24 17:03:43 +0000

    chore(app): refactors the location card components into a shared component

---
+++ services/app/src/modules/locations/components/LocationCard/LocationCard.tsx
@@ -0,0 +1,37 @@
+import { Card, Image, Text, Group, Button, ActionIcon } from '@mantine/core'
+
+import { Location } from '@/graphql'
+
+type LocationCardProps = {
+  location: Location
+  compact?: boolean
+}
+
+export const LocationCard = (props: LocationCardProps) => {
+  return (
+    <Card shadow='sm' padding='lg' radius='sm' withBorder>
+      {!props.compact ? (
+        <Card.Section>
+          <Image
+            src='https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
+            height={160}
+            alt='Norway'
+          />
+        </Card.Section>
+      ) : null}
+
+      <Text weight={800} size='lg' mt={!props.compact ? 'lg' : undefined}>{props.location?.name}</Text>
+      <Text size='sm' italic={!props.location?.description}>{props.location?.description || 'No description provided.'}</Text>
+
+      {/* <Group mt='xs'>
+        <Button radius='md' style={{ flex: 1 }}>
+          View Inventory
+        </Button>
+      </Group> */}
+
+      {/* <Group mt='md'>
+        <Text fz='xs' opacity={0.45}>{edge.node?.name} → {child.name}</Text>
+      </Group> */}
+    </Card>
+  )
+}
--- services/app/src/modules/locations/components/LocationsGrid/LocationsGrid.tsx
+++ services/app/src/modules/locations/components/LocationsGrid/LocationsGrid.tsx
@@ -1,9 +1,10 @@
-import { Grid, Card, Image, Text, Stack, Group, Button, ActionIcon } from '@mantine/core'
+import { Grid, Card, Image, Text, Stack, Group, Button, ActionIcon, Avatar } from '@mantine/core'
 import { BiStar } from 'react-icons/bi'
 
-import { LocationConnection, LocationEdge } from '@/graphql'
+import { Location, LocationConnection } from '@/graphql'
 
 import { LocationsGridGroup } from './LocationsGridGroup'
+import { LocationCard } from '../LocationCard/LocationCard'
 
 type LocationsGridProps = {
   locations: LocationConnection
@@ -26,40 +27,16 @@ export const LocationsGrid = (props: LocationsGridProps) => {
         >
           <Grid>
             <Grid.Col span={4}>
-              <Card shadow='sm' padding='lg' radius='sm' withBorder>
-                <Card.Section>
-                  <Image
-                    src='https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
-                    height={160}
-                    alt='Norway'
-                  />
-                </Card.Section>
-
-                <Text weight={800} size='lg' mt='lg'>{edge?.node?.name}</Text>
-                <Text size='sm' italic={!edge?.node?.description}>{edge?.node?.description || 'No description provided.'}</Text>
-
-                <Group mt='xs'>
-                  <Button radius='md' style={{ flex: 1 }}>
-                    View Inventory
-                  </Button>
-                  <ActionIcon variant='default' radius='md' size={36}>
-                    <BiStar />
-                  </ActionIcon>
-                </Group>
-              </Card>
+              <LocationCard
+                location={edge?.node as Location}
+              />
             </Grid.Col>
 
             <Grid.Col span={8}>
               <Grid>
                 {edge?.node?.children?.map((child) => (
                   <Grid.Col span={4}>
-                    <Card shadow='sm' padding='lg' radius='sm' withBorder>
-                      <Text weight={800} size='lg'>{child.name}</Text>
-                      <Text size='sm' italic={!child.description}>{child.description || 'No description provided.'}</Text>
-                      <Group mt='md'>
-                        <Text fz='xs' opacity={0.45}>{edge.node?.name} → {child.name}</Text>
-                      </Group>
-                    </Card>
+                    <LocationCard location={child} compact />
                   </Grid.Col>
                 ))}
               </Grid>
@@ -75,18 +52,7 @@ export const LocationsGrid = (props: LocationsGridProps) => {
         <Grid>
           {rootLocationsWithoutChildren?.map((edge) => (
             <Grid.Col span={4}>
-              <Card shadow='sm' padding='lg' radius='sm' withBorder>
-                <Card.Section>
-                  <Image
-                    src='https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
-                    height={160}
-                    alt='Norway'
-                  />
-                </Card.Section>
-
-                <Text weight={800} size='lg' mt='lg'>{edge?.node?.name}</Text>
-                <Text size='sm' italic={!edge?.node?.description}>{edge?.node?.description || 'No description provided.'}</Text>
-              </Card>
+              <LocationCard location={edge?.node as Location} />
             </Grid.Col>
           ))}
         </Grid>
